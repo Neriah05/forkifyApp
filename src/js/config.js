c3605ced7b8ef287e506663af3edab87818f62d0
Neriah05 <neriahuriri36@gmail.com>
2023-07-12 23:37:37 +0100

    completed forkify webapp

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '271147d6-6c33-41d7-a672-1582d73252a1';
+export const MODAL_CLOSE_SEC = 2.5;
